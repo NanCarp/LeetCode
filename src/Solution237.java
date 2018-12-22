9bb127f3b41ed7e2db2e6df36d65d12424ab8d75
nanliyu <nancarpli@163.com>
2018-12-22 10:08:13 +0800

    237. 删除链表中的节点

---
+++ src/Solution237.java
@@ -0,0 +1,28 @@
+/**
+ * Created by nanca on 12/22/2018.
+ */
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ * int val;
+ * ListNode next;
+ * ListNode(int x) { val = x; }
+ * }
+ */
+public class Solution237 {
+
+    public void deleteNode(ListNode node) {
+        node.val = node.next.val;
+        node.next = node.next.next;
+    }
+}
+
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode(int x) {
+        val = x;
+    }
+}
