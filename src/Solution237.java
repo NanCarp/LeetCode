da0c8c5c224a817a5d9e21d2b1fde3904c0931d8
nanliyu <nancarpli@163.com>
2018-12-24 22:55:05 +0800

    237. 删除链表中的节点

--- src/Solution237.java
+++ src/Solution237.java
@@ -16,13 +16,14 @@ public class Solution237 {
         node.val = node.next.val;
         node.next = node.next.next;
     }
-}
 
-class ListNode {
-    int val;
-    ListNode next;
+    class ListNode {
+        int val;
+        ListNode next;
 
-    ListNode(int x) {
-        val = x;
+        ListNode(int x) {
+            val = x;
+        }
     }
 }
+
