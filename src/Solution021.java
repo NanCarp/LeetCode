925b7a034ef6b8ebfdbc0c67f6c6b8705d84841b
NanCarp <nancarpli@163.com>
2019-05-22 22:07:45 +0800

    21. 合并两个有序链表

---
+++ src/Solution021.java
@@ -0,0 +1,22 @@
+import entity.ListNode;
+
+/**
+ * 21. 合并两个有序链表
+ */
+public class Solution021 {
+
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        if (l1 == null) return l2;
+        if (l2 == null) return l1;
+
+        ListNode head = null;
+        if (l1.val <= l2.val){
+            head = l1;
+            head.next = mergeTwoLists(l1.next, l2);
+        } else {
+            head = l2;
+            head.next = mergeTwoLists(l1, l2.next);
+        }
+        return head;
+    }
+}
